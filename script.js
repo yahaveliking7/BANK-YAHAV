5e19d1b7ee577d0173cc01ce8a549e0d8c5e321a
yahaveliking7 <yahaveli796@gmail.com>
2024-09-01 21:52:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let total=document.getElementById('total')
+let amount=0
+ function add(){
+    amount=amount+1
+total.innerHTML=amount
+
+ }
+ function reduce(){
+    amount=amount-1
+total.innerHTML=amount
+ }
+ function zero(){
+    amount=amount=0
+    total.innerHTML=amount
+ }
+ function dubel(){
+    amount=amount*2
+    total.innerHTML=amount
+ }
